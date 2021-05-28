fe405d6bd2b52dbf0ff703aee4cc8139c89cc4b4
darkvoidSH <81687622+darkvoidSH@users.noreply.github.com>
2021-05-28 22:05:07 +0200

    aggiunte 3060 tolte alcune 3070

--- monitor.js
+++ monitor.js
@@ -1,7 +1,7 @@
 const axios = require('axios').default;
 var qs = require('qs');
 const discord = require('./sendDiscord.js');
-var a,b,c,d,e,f,g = false;
+var a,b,c,d,e = false;
 
 
 async function sleep(ms) {
@@ -9,21 +9,21 @@ async function sleep(ms) {
 }
 
 async function monitor() { 
-
+    var init = new Date().getTime()
     axios.all([
+        axios.get(`https://www.mediaworld.it/api/v2/wcs/resources/store/20000/productview/withPartNumbersAndFilter/p-157528`), //asus dual 3060
+        axios.get(`https://www.mediaworld.it/api/v2/wcs/resources/store/20000/productview/withPartNumbersAndFilter/p-153357`), //asus tuf 3060
         axios.get(`https://www.mediaworld.it/api/v2/wcs/resources/store/20000/productview/withPartNumbersAndFilter/p-149212`), //3060ti
         axios.get(`https://www.mediaworld.it/api/v2/wcs/resources/store/20000/productview/withPartNumbersAndFilter/p-145164`), //asus 3070 biventola 
         axios.get(`https://www.mediaworld.it/api/v2/wcs/resources/store/20000/productview/withPartNumbersAndFilter/p-145091`), //asus 3070 TUF triventola
-        axios.get(`https://www.mediaworld.it/api/v2/wcs/resources/store/20000/productview/withPartNumbersAndFilter/p-149220`), //asus rog strix 3070
-        axios.get(`https://www.mediaworld.it/api/v2/wcs/resources/store/20000/productview/withPartNumbersAndFilter/p-152538`), //msi 3070
-        axios.get(`https://www.mediaworld.it/api/v2/wcs/resources/store/20000/productview/withPartNumbersAndFilter/p-152543`) //msi triventola 3070
+
         ])
-        .then(axios.spread((data1,data2,data3,data4,data5,data6) => {
+        .then(axios.spread((data1,data2,data3,data4,data5) => {
             console.log("[+] checking...")
             if(!a){
                 if(data1.data['CatalogEntryView'][0]['AvailabilityMap']['quantity']>0){
                     console.log("[+] Available")
-                    discord.sendToDiscord("https://www.mediaworld.it/product/p-149212/asus-dual-geforce-rtx-3060-ti-oc-edition")
+                    discord.sendToDiscord("https://www.mediaworld.it/product/p-157528/asus-dual-geforce-rtx-3060-12gb")
                     a=true;
                 }
             }
@@ -31,7 +31,7 @@ async function monitor() {
             if(!b){
                 if(data2.data['CatalogEntryView'][0]['AvailabilityMap']['quantity']>0){
                     console.log("[+] Available")
-                    discord.sendToDiscord("https://www.mediaworld.it/product/p-145164/asus-geforce-rtx-3070-8gb")
+                    discord.sendToDiscord("https://www.mediaworld.it/product/p-153357/asus-tuf-geforce-rtx-3060-12gb")
                     b=true
                 }
             }
@@ -39,7 +39,7 @@ async function monitor() {
             if(!c){
                 if(data3.data['CatalogEntryView'][0]['AvailabilityMap']['quantity']>0){
                     console.log("[+] Available")
-                    discord.sendToDiscord("https://www.mediaworld.it/product/p-145091/asus-tuf-geforce-rtx-3070-8gb")
+                    discord.sendToDiscord("https://www.mediaworld.it/product/p-149212/asus-dual-geforce-rtx-3060-ti-oc-edition")
                 }
                 c=true
             }
@@ -47,7 +47,7 @@ async function monitor() {
             if(!d){
                 if(data4.data['CatalogEntryView'][0]['AvailabilityMap']['quantity']>0){
                     console.log("[+] Available")
-                    discord.sendToDiscord("https://www.mediaworld.it/product/p-149220/asus-rog-strix-geforce-rtx-3070-8gb")
+                    discord.sendToDiscord("https://www.mediaworld.it/product/p-145164/asus-geforce-rtx-3070-8gb")
                     d=true
                 }
             }
@@ -55,19 +55,11 @@ async function monitor() {
             if(!e){
                 if(data5.data['CatalogEntryView'][0]['AvailabilityMap']['quantity']>0){
                     console.log("[+] Available")
-                    discord.sendToDiscord("https://www.mediaworld.it/product/p-152538/msi-geforce-rtx3070-ventus-2x-oc")
+                    discord.sendToDiscord("https://www.mediaworld.it/product/p-145091/asus-tuf-geforce-rtx-3070-8gb")
                     e=true
                 }
             }
-
-            if(!f){
-                if(data6.data['CatalogEntryView'][0]['AvailabilityMap']['quantity']>0){
-                    console.log("[+] Available")
-                    discord.sendToDiscord("https://www.mediaworld.it/product/p-152543/msi-geforce-rtx3070-ventus-3x-oc")
-                    f=true
-                }
-            }
-            setTimeout(monitor, 1500);
+            setTimeout(monitor, 1000);
         }));
 }
 
